56f7555f4e48f469f97e74ee471d128b9218453c
AndreiTiberiu <andreibanica16@gmail.com>
2024-09-16 13:10:06 +0300

    first commit

---
+++ Project-Store-FrontEnd-master/src/environments/environment.ts
@@ -0,0 +1,9 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "Legumeez",
+  appOwner: "Banica Andrei Tiberiu",
+  appLogo: "../assets/logo.jpg",
+  itemCategories: ['SMALL', 'MEDIUM', 'LARGE']
+};
